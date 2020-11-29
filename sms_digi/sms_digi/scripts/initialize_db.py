8162cd125fb55442643423d23f2f8c27119c930b
neha_garde <neha_garde@persistent.com>
2020-11-29 15:54:02 +0530

    Initialize db script

--- sms_digi/sms_digi/scripts/initialize_db.py
+++ sms_digi/sms_digi/scripts/initialize_db.py
@@ -1,42 +1,39 @@
 import argparse
 import sys
 
-from pyramid.paster import bootstrap, setup_logging
+from pyramid.paster import bootstrap, setup_logging, get_appsettings
+from sqlalchemy import engine_from_config
 from sqlalchemy.exc import OperationalError
 
 from .. import models
 
 
-def setup_models(dbsession):
+def setup_models(db, dbsession):
+    from sms_digi.models.meta import metadata
+    metadata.bind = db
+    metadata.create_all()
 
-    dbsession.execute(
-        # user data
-        """
-        INSERT INTO public.user( email, password) VALUES ('neha.r.garde@gmail.com', 'Pass@123');
-        """
+    super_admin = models.User(email="neha.r.garde@gmail.com", password="Pass@123")
 
-        # chemical data
-        """
-        INSERT INTO public.chemical(name) VALUES ('C');
-        INSERT INTO public.chemical(name) VALUES ('N');
-        INSERT INTO public.chemical(name) VALUES ('O');
-        INSERT INTO public.chemical(name) VALUES ('AI');
-        """
-
-        # commodity data
-        """
+    dbsession.add(super_admin)
+    dbsession.execute("""INSERT INTO public.chemical(name) VALUES ('C');
+                        INSERT INTO public.chemical(name) VALUES ('N');
+                        INSERT INTO public.chemical(name) VALUES ('O');
+                        INSERT INTO public.chemical(name) VALUES ('AI');
+                        INSERT INTO public.chemical(id,name) VALUES (0,'UNKNOWN');""")
+    dbsession.execute(""" 
         INSERT INTO public.commodity(name, inventory, price, chemical_composition) 
         VALUES ('Plate & Structural', 200, 20.5,'[ {"id": 1, "percentage": 25} ,
                                                     {"id": 2, "percentage": 25},
-                                                    {"unknown": 50}]' );
+                                                    {"id": 0, "percentage": 50}]' );
 
         INSERT INTO public.commodity(name, inventory, price, chemical_composition) 
         VALUES ('Plates', 2000, 20.5,'[ {"id": 1, "percentage": 25} ,
                                                     {"id": 2, "percentage": 25},
-                                                    {"unknown": 50}]' );
+                                                    {"id": 0, "percentage": 50}]' );
 
         """)
-
+    print("Database initialized")
 
 def parse_args(argv):
     parser = argparse.ArgumentParser()
@@ -51,11 +48,18 @@ def main(argv=sys.argv):
     args = parse_args(argv)
     setup_logging(args.config_uri)
     env = bootstrap(args.config_uri)
-
+    settings = get_appsettings(args.config_uri)
     try:
+        db = engine_from_config(settings, prefix='sqlalchemy.')
+        print("Database initializing in-progress.")
+        print(settings)
+        import configparser
+        config = configparser.ConfigParser()
+        config.read(args.config_uri)
+
         with env['request'].tm:
             dbsession = env['request'].dbsession
-            setup_models(dbsession)
+            setup_models(db, dbsession)
     except OperationalError:
         print('''
 Pyramid is having a problem using your SQL database.  The problem
